5961b1d5dca32ec15b8f422133ffb4a1cd0027d7
Momen Fnnana <mommen@hemma.sa>
2022-02-08 20:52:52 +0200

    navigation structure, loading assets and fonts before rendering the app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { ThemeProvider } from "@shopify/restyle";
+import {
+  Onboarding,
+  Welcome,
+  assets as AuthenTicationAssets,
+} from "./src/Authentication";
+import { LoadAssets, theme } from "./src/components";
+import { AuthRoutes } from "./src/components/Navigation";
+
+const AuthenticationStack = createNativeStackNavigator<AuthRoutes>();
+const assets = [...AuthenTicationAssets];
+const fonts = {
+  "SFProText-Bold": require("./assets/fonts/SFProText-Bold.ttf"),
+  "SFProText-Regular": require("./assets/fonts/SFProText-Regular.ttf"),
+  "SFProText-Semibold": require("./assets/fonts/SFProText-Semibold.ttf"),
+  "SFProText-Medium": require("./assets/fonts/SFProText-Medium.ttf"),
+};
+
+const AuthenticationNavigator = () => {
+  return (
+    <AuthenticationStack.Navigator
+      initialRouteName="Onboarding"
+      screenOptions={{ headerShown: false }}
+    >
+      <AuthenticationStack.Screen name="Onboarding" component={Onboarding} />
+      <AuthenticationStack.Screen name="Welcome" component={Welcome} />
+    </AuthenticationStack.Navigator>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider {...{ theme }}>
+      <LoadAssets {...{ fonts, assets }}>
+        <AuthenticationNavigator />
+      </LoadAssets>
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
